a73b118fd922c342840edcb6e03712bf67e20fec
caily <wuqi@terminus.io>
2017-10-09 21:56:53 +0800

    modify

--- src/main/kotlin/com/jiaoye/caily/downloader/interceptor/LoginInterceptor.kt
+++ src/main/kotlin/com/jiaoye/caily/downloader/interceptor/LoginInterceptor.kt
@@ -7,16 +7,21 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 open class LoginInterceptor : HandlerInterceptor {
+
+
     override fun preHandle(request: HttpServletRequest?, response: HttpServletResponse?, handler: Any?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+
+        return true
     }
 
     override fun postHandle(request: HttpServletRequest?, response: HttpServletResponse?, handler: Any?, modelAndView: ModelAndView?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun afterCompletion(request: HttpServletRequest?, response: HttpServletResponse?, handler: Any?, ex: Exception?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
